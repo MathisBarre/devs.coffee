7c81307e9ceb7fe6ac9e34bc69b48022c75bc299
Mathis Barré <mathis.barre@live.fr>
2020-11-28 15:13:16 +0100

    minor layout fix

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="text-center mt-8 py-6 font-semibold bg-gray-100 text-md">
       <div className="flex flex-col lg:flex-row items-center justify-between max-w-7xl mx-auto px-4">
-        <p className="mb-2"><a className="underline" href="https://mathisbarre.com">Créé avec ❤️ par Mathis Barré</a></p>
+        <p className="mb-2 lg:mb-0"><a className="underline" href="https://mathisbarre.com">Créé avec ❤️ par Mathis Barré</a></p>
         <div>
           <a href="https://fr.reactjs.org/"><img className="inline h-9 mx-3 my-2" src="/images/react.png" alt="react.js"/></a>
           <a href="https://nextjs.org"><img className="inline h-10 mx-3 my-2" src="/images/nextjs.svg" alt="next.js"/></a>
