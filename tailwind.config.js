2406094718db44ecdb4b7fb452186b74a7c686a9
Mathis Barré <mathis.barre@live.fr>
2020-11-28 11:33:55 +0100

    add tailwindcss purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './**/*.html',
+    './**/*.js',
+    './**/*.jsx',
+    './**/*.tsx',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
