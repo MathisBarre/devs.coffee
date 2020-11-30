aadd5979edd594562d2e15e21eb1c6972a34fa1b
Mathis Barré <mathis.barre@live.fr>
2020-11-30 09:21:23 +0100

    add meta description & html lang attribute

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,7 +2,6 @@ import Head from "next/head"
 import Header from "../components/layout/Header.jsx"
 import Footer from "../components/layout/Footer.jsx"
 
-import "tailwindcss/tailwind.css"
 import "../style/global.scss"
 
 function App({ Component, pageProps }) {
@@ -10,6 +9,7 @@ function App({ Component, pageProps }) {
     <>
       <Head>
         <title>dev.cafe</title>
+        <meta name="description" content="dev.cafe est un site qui a pour objectif d’augmenter l'expérience étudiant chez OpenClassrooms via des ressources additionnelles et une liste de questions fréquentes ainsi que d’améliorer les liens communautaires grâce à liste de discord, une carte des étudiants et un calendrier des rendez-vous à ne pas louper !"/>
       </Head>
       <Header/>
       <Component {...pageProps} />
