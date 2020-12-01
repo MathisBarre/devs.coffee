ab8a2128b162159c190d6ca2c8ef1744ebb87306
Mathis Barré <mathis.barre@live.fr>
2020-12-01 22:27:10 +0100

    Add google analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,25 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import Head from "next/head"
+import * as gtag from 'lib/gtag'
+
 import Header from "components/layout/Header.jsx"
 import Footer from "components/layout/Footer.jsx"
 
 import "../style/global.scss"
 
 function App({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <Head>
