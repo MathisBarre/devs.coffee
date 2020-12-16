0501a685645eb47fd6a676cbf78f03b6510ab21d
Mathis Barré <mathis.barre@live.fr>
2020-12-16 14:10:36 +0100

    feat: map loader

--- components/StudentMap.jsx
+++ components/StudentMap.jsx
@@ -12,10 +12,13 @@ export default function Map() {
         }}
         withContentPadding={false}
       >
-      <iframe 
-        src="https://www.google.com/maps/d/embed?mid=137K62Pc-8FSmvznlKPAap4USgP8Wz0ob" 
-        className="w-full max-w-full h-144 mt-4 rounded-md"
-      />
+      <div className="w-full max-w-full h-144 mt-4 relative">
+        <div className="rounded-md animate-pulse bg-gray-200 dark:bg-gray-1100 absolute h-full w-full"></div>
+        <iframe 
+          src="https://www.google.com/maps/d/embed?mid=137K62Pc-8FSmvznlKPAap4USgP8Wz0ob" 
+          className="absolute w-full h-full rounded-md animate-none"
+        />
+      </div>
       </Card>
     </section>
   );
