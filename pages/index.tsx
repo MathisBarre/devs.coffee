c9b4bbf74c66266d5d097ef93ca496d8b014191c
Mathis Barré <mathis.barre@live.fr>
2021-02-27 11:23:09 +0100

    feat: publicationCard component & dark mod

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import HomepageCard from 'components/HomepageCard'
-import Link from 'next/link'
+import PublicationCard from 'components/PublicationCard'
 
 export default function Index() {
   return (
@@ -35,159 +35,39 @@ export default function Index() {
         />
       </div>
       <div className="grid gap-8 lg:grid-cols-3 lg:max-w-none w-full mt-8">
-        <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-          <div className="flex-shrink-0">
-            <img
-              className="h-48 w-full object-cover"
-              src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixqx=pLPhGoGaS9&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
-              alt=""
-            />
-          </div>
-          <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-            <div className="flex-1">
-              <p className="text-sm font-medium text-indigo-600">
-                <a href="#" className="hover:underline">
-                  Article
-                </a>
-              </p>
-              <a href="#" className="block mt-2">
-                <p className="text-xl font-semibold text-gray-900">Boost your conversion rate</p>
-                <p className="mt-3 text-base text-gray-500">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium
-                  praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.
-                </p>
-              </a>
-            </div>
-            <div className="mt-6 flex items-center">
-              <div className="flex-shrink-0">
-                <a href="#">
-                  <span className="sr-only">Roel Aufderehar</span>
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=pLPhGoGaS9&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                </a>
-              </div>
-              <div className="ml-3">
-                <p className="text-sm font-medium text-gray-900">
-                  <a href="#" className="hover:underline">
-                    Roel Aufderehar
-                  </a>
-                </p>
-                <div className="flex space-x-1 text-sm text-gray-500">
-                  <time dateTime="2020-03-16">Mar 16, 2020</time>
-                  <span aria-hidden="true">·</span>
-                  <span>6 min read</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-          <div className="flex-shrink-0">
-            <img
-              className="h-48 w-full object-cover"
-              src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=pLPhGoGaS9&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
-              alt=""
-            />
-          </div>
-          <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-            <div className="flex-1">
-              <p className="text-sm font-medium text-indigo-600">
-                <a href="#" className="hover:underline">
-                  Video
-                </a>
-              </p>
-              <a href="#" className="block mt-2">
-                <p className="text-xl font-semibold text-gray-900">
-                  How to use search engine optimization to drive sales
-                </p>
-                <p className="mt-3 text-base text-gray-500">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores
-                  porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio
-                  animi., tempore temporibus quo laudantium.
-                </p>
-              </a>
-            </div>
-            <div className="mt-6 flex items-center">
-              <div className="flex-shrink-0">
-                <a href="#">
-                  <span className="sr-only">Brenna Goyette</span>
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixqx=pLPhGoGaS9&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                </a>
-              </div>
-              <div className="ml-3">
-                <p className="text-sm font-medium text-gray-900">
-                  <a href="#" className="hover:underline">
-                    Brenna Goyette
-                  </a>
-                </p>
-                <div className="flex space-x-1 text-sm text-gray-500">
-                  <time dateTime="2020-03-10">Mar 10, 2020</time>
-                  <span aria-hidden="true">·</span>
-                  <span>4 min read</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-          <div className="flex-shrink-0">
-            <img
-              className="h-48 w-full object-cover"
-              src="https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixqx=pLPhGoGaS9&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
-              alt=""
-            />
-          </div>
-          <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-            <div className="flex-1">
-              <p className="text-sm font-medium text-indigo-600">
-                <a href="#" className="hover:underline">
-                  Case Study
-                </a>
-              </p>
-              <a href="#" className="block mt-2">
-                <p className="text-xl font-semibold text-gray-900">
-                  Improve your customer experience
-                </p>
-                <p className="mt-3 text-base text-gray-500">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum
-                  voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque
-                  corporis perferendis hic.
-                </p>
-              </a>
-            </div>
-            <div className="mt-6 flex items-center">
-              <div className="flex-shrink-0">
-                <a href="#">
-                  <span className="sr-only">Daniela Metz</span>
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixqx=pLPhGoGaS9&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                </a>
-              </div>
-              <div className="ml-3">
-                <p className="text-sm font-medium text-gray-900">
-                  <a href="#" className="hover:underline">
-                    Daniela Metz
-                  </a>
-                </p>
-                <div className="flex space-x-1 text-sm text-gray-500">
-                  <time dateTime="2020-02-12">Feb 12, 2020</time>
-                  <span aria-hidden="true">·</span>
-                  <span>11 min read</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <PublicationCard
+          image="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=pLPhGoGaS9&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
+          type="Article"
+          title="Boost yout conversation rate"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum."
+          author={{
+            name: 'Roel Aufderehar',
+            profilePictureUrl:
+              'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=pLPhGoGaS9&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+          }}
+        />
+        <PublicationCard
+          image="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=pLPhGoGaS9&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
+          type="Article"
+          title="Boost yout conversation rate"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum."
+          author={{
+            name: 'Roel Aufderehar',
+            profilePictureUrl:
+              'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=pLPhGoGaS9&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+          }}
+        />
+        <PublicationCard
+          image="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=pLPhGoGaS9&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
+          type="Article"
+          title="Boost yout conversation rate"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum."
+          author={{
+            name: 'Roel Aufderehar',
+            profilePictureUrl:
+              'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=pLPhGoGaS9&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+          }}
+        />
       </div>
     </div>
   )
