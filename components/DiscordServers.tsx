720b7803596ee033086a2982bbaadce8f7853a5e
Mathis Barré <m.barre@beapp.fr>
2022-02-01 14:43:47 +0100

    feat(perf): download discord server image locally

--- components/DiscordServers.tsx
+++ components/DiscordServers.tsx
@@ -8,7 +8,7 @@ const discordServers = [
     courses: ['WEB'],
     description: "Ici on se connait tous entre nous ! On discute et on s'entraide sans se prendre la tête !",
     href: 'https://discord.gg/f6BsPK9',
-    img: 'https://cdn.discordapp.com/icons/766565028312383498/15f8f84b303cc2e779b1ef2c17b705e6.webp?size=256',
+    img: '/images/discord-servers/devscoffee.webp',
     api: 'https://discord.com/api/guilds/766565028312383498/widget.json'
   },
   {
@@ -16,7 +16,7 @@ const discordServers = [
     courses: ['ALL'],
     description: 'Serveur général regroupant toute la communauté OpenClassrooms française.',
     href: 'https://discord.gg/u7dxx3K',
-    img: 'https://cdn.discordapp.com/icons/595235640044552223/54a7b93bc8202aadb8aa0d1289b59435.webp?size=128',
+    img: '/images/discord-servers/sansprisedetech.webp',
     api: 'https://discord.com/api/guilds/595235640044552223/widget.json'
   },
   {
@@ -24,7 +24,7 @@ const discordServers = [
     courses: ['WEB'],
     description: 'Discussions & entraide parcours développeur web, ainsi que des channels de discussion post-formation (emplois, stacks, projets hors formation).',
     href: 'https://discord.gg/DPyyzrT',
-    img: 'https://cdn.discordapp.com/icons/651031474966888458/1b2efcc2293cddb82b254022f6350d90.webp?size:256',
+    img: '/images/discord-servers/devweb.webp',
     api: 'https://discord.com/api/guilds/651031474966888458/widget.json'
   },
   {
@@ -32,7 +32,7 @@ const discordServers = [
     courses: ['FRONT_END'],
     description: 'Discussions, entraide et soutien autour du parcours développeur web / développeur web front-end et de ses projets.',
     href: 'https://discord.gg/SKJVyew',
-    img: 'https://cdn.discordapp.com/icons/569020625776017410/12d51a5f472cbf0e5b17371469975f3e.webp?size:256',
+    img: '/images/discord-servers/da.webp',
     api: 'https://discord.com/api/guilds/569020625776017410/widget.json'
   },
   {
@@ -40,7 +40,7 @@ const discordServers = [
     courses: ['ALL'],
     description: "training-dev est un site de cours/tuto/live/exercices dans le domaine du développement web. Le discord est un lieu d'échange pour les débutants comme les professionnels à tout ce qui touche au développement web ou au métiers du développement. Venez y discuter de vos formations, vos projets, vos difficultés ou réussites et partagez vos expériences.",
     href: 'https://discord.gg/DweDNNcJfN',
-    img: 'https://cdn.discordapp.com/icons/788399435662622720/e378d416fac65d3fe1fe8aee12de24d6.webp?size=256'
+    img: '/images/discord-servers/trainingdev.webp'
   }
 ]
 
